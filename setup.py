968dd26a982c64eb9b6e2e02c121f8fb5998f8e8
BrookeEB <bebart1911@ung.edu>
2018-08-01 13:39:09 -0400

    Created a requirement file.

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from distutils.core import setup
+import py2exe, sys, os
 
-import py2exe
+sys.argv.append('py2exe')
 
-setup(console=['GUICalc.py'])
+setup(windows=[{'script': 'filename.py'}], \
+            options={"py2exe": {"includes": ["decimal", "Tkinter", \
+            "tkFileDialog", "csv", "xml.dom.minidom", "os"], \
+            'bundle_files': 1, 'compressed': False}}, \
+            zipfile = None)
+
+self.dlls_in_exedir = [python_dll,
+                       "w9xpopen%s.exe" % (is_debug_build and "_d" or ""),
+                       "msvcr71%s.dll" % (is_debug_build and "d" or ""),
+                       "tcl85.dll",
+                       "tk85.dll"]
